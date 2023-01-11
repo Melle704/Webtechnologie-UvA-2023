9a5a1767f07e581340242f69c761e7529e259e38
Nicolas Mazzon <nicolasmazzon549@gmail.com>
2023-01-11 22:46:44 +0100

    Header elements alignment.

--- html/cards.php
+++ html/cards.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>MTG | Deck building</title>
+    <title>MTG | Cards</title>
 
     <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
 	<link rel="stylesheet" type="text/css" href="/css/style.css">
@@ -36,7 +36,7 @@
     </div>
 
     <div>
-        home page
+        cards page
     </div>
 </body>
 
--- html/register.php
+++ html/register.php
@@ -19,11 +19,18 @@
                 <span>MAGIC</span> THE GATHERING
             </div>
             <div class="box-row">
-                <ul>
-                    <li><a href="index.php">Index</a></li>
-                    <li><a href="register.php">Register</a></li>
-                    <li><a href="login.php">Login</a></li>
-                </ul>
+                <div class="box-left">
+                    <ul>
+                        <li><a href="index.php">Index</a></li>
+                        <li><a href="cards.php">Cards</a></li>
+                    </ul>
+                </div>
+                <div class="box-right">
+                    <ul>
+                        <li><a href="login.php">Login</a></li>
+                        <li><a href="register.php">Register</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
