f4c8db48bff1a34fd702e6b6d2d623ab224c05b7
Nicolas Mazzon <nicolasmazzon549@gmail.com>
2023-01-11 18:39:13 +0100

    Title and trailing \

--- html/login.php
+++ html/login.php
@@ -5,10 +5,10 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Webtechnologie voor KI/INF 2023</title>
+    <title>MTG | Login</title>
 
     <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
-	<link rel="stylesheet" type="text/css" href="/css/style.css" />
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script> 
 </head>
 
