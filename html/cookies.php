3b55087101f2d75c57e2de14b0b53fc9c0fa53c3
Kas Visser <kas.visser@outlook.com>
2023-01-17 11:15:53 +0100

    Updated cookie policy and fixed a small issue.

--- html/cookies.php
+++ html/cookies.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>MTG | Cards</title>
+    <title>MTG | Cookies</title>
 
     <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
 	<link rel="stylesheet" type="text/css" href="/css/style.css">
@@ -16,8 +16,12 @@
 <?php include_once "header.php";?>
 
 <div class="box box-row">
-    <p>We use cookies to recognise your computer when you visit our website, in order to recognise wether you are logged in or not.</p>
-    
+    <div class="box-row box-light">
+        <b>Cookie policy</b>
+    </div>
+    <p>We use cookies on our website. By using our website or registering you consent to our use of cookies in accordance with our terms of policy.</p>
+    <p>We only use session cookies on our website. We use these cookies to recognise your computer when you visit our website, in order to recognise whether you are loggin in or not. We only do this if you have registered as a user.</p>
+    <p>A cookie is in no way giving us acces to your computer or any other information about you.</p>
     <p>If you would like to find out more about cookies, including how to manage them, please visit: <a href="https://www.allaboutcookies.org">allaboutcookies.org</a></p>
 </div>
 
