62ff19bf771754a7eda6dc7d5b6c92fc37785ad1
Kas Visser <kas.visser@outlook.com>
2023-01-20 16:46:58 +0100

    Improved styling, added divs and added text wrapping.

--- html/forum.php
+++ html/forum.php
@@ -17,42 +17,57 @@
     <?php include_once "header.php";?>
 
     <div class="box box-row">
-        <ol>
-            <li class="row">
-                <div class="preview-box">
-                    <a href="forumpost.php">
-                        <h class="preview-title"> 
-                            Hallo dit is de test titel.
-                        </h>
-                        <div class="bottom-text">
-                            <p class="post-timestamp">
-                                1-1-1984
-                            </p>
-                            <p class="comment-count">
-                                3 comments
-                            </p>
-                        </div>
-                    </a>
+
+        <h class="post-title">
+            Magic the Gathering forum
+        </h>
+        <p>A space to ask questions and discuss Magic! <a href="forumrules.php">Forum rules</a></p>
+
+        <div class="preview-box">
+            <a href="forumpost.php">
+                <h class="preview-title"> 
+                    Hallo dit is de test titel.
+                </h>
+                <div class="bottom-text">
+                    <p class="post-timestamp">
+                        1-1-1984
+                    </p>
+                    <p class="comment-count">
+                        3 comments
+                    </p>
                 </div>
-            </li>
-            <li class="row">
-                <div class="preview-box">
-                    <a href="forumpost.php">
-                        <h class="preview-title"> 
-                            En dit is dan de tweede post!
-                        </h>
-                        <div class="bottom-text">
-                            <p class="post-timestamp">
-                                1-1-1984
-                            </p>
-                            <p class="comment-count">
-                                2 comments
-                            </p>
-                        </div>
-                    </a>
+            </a>
+        </div>
+        <div class="preview-box">
+            <a href="forumpost.php">
+                <h class="preview-title"> 
+                    En dit is dan de tweede post!
+                </h>
+                <div class="bottom-text">
+                    <p class="post-timestamp">
+                        1-1-1984
+                    </p>
+                    <p class="comment-count">
+                        2 comments
+                    </p>
                 </div>
-            </li>
-        </ol>
+            </a>
+        </div>
+        <div class="preview-box">
+            <a href="forumpost.php">
+                <h class="preview-title"> 
+                    En dit is dan de derde post!
+                </h>
+                <div class="bottom-text">
+                    <p class="post-timestamp">
+                        1-1-1984
+                    </p>
+                    <p class="comment-count">
+                        4 comments
+                    </p>
+                </div>
+            </a>
+        </div>
     </div>
 
     <?php include_once "footer.php"; ?>
