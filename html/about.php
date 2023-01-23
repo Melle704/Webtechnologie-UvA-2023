e3e2c1c831ccac5ccf3dd47c75e13082f16b7892
Kas <88986990+Melle704@users.noreply.github.com>
2023-01-23 23:19:33 +0100

    Added content to the about page.

--- html/about.php
+++ html/about.php
@@ -12,17 +12,21 @@
 </head>
 
 <body>
+    <?php include_once "header.php";?>
 
-<?php include_once "header.php";?>
+    <div class="box">
+        <div class="box-row box-light">
+            <b>About us</b>
+        </div>
+        <div class="box-row">
+            <p>The Grey Lotus is an online Magic the Gathering store for players, by players. We are a group of students from the University of Amsterdam that are working on the website as an assignment for our Webtech course.</p> 
+            <p>You can search for single cards to purchase, discuss it on our forum or try building a deck with it in our deckbuilder!</p>
+            <p>Everybody can make a personal profile, where you can show your current collection of cards for trading or just to brag.</p>
+            <p><b>Have fun and see you at FNM!</b></p>
+        </div>
+    </div>
 
-<div class="box box-row">
-    <p>
-    Fakka Gangsters!
-    </p>
-</div>
-
-<?php include_once "footer.php"; ?>
-
+    <?php include_once "footer.php"; ?>
 </body>
 
 </html>
