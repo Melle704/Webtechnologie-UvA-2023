a753d1d8901c5b00cff21c8066dfe9f1d86ff24e
ceylansen <117072022+ceylansen@users.noreply.github.com>
2023-01-23 14:52:51 +0100

    javascript voor passwords

---
+++ html/js/ShowPassword.js
@@ -0,0 +1,14 @@
+function ShowPassword() {
+    var x = document.getElementById("password1");
+        if (x.type === "password") {
+        x.type = "text";
+        } else {
+        x.type = "password";
+        }
+        var y = document.getElementById("password2");
+        if (y.type === "password") {
+        y.type = "text";
+        } else {
+        y.type = "password";
+        }
+}
