34968c4ad11542b532e38d17f41f070e3444bfb4
WINSDK <nicolasmazzon549@gmail.com>
2023-01-23 16:14:29 -0800

    Formatting password match.

--- html/js/MatchPassword.js
+++ html/js/MatchPassword.js
@@ -1,10 +1,10 @@
 var check = function() {
-    if (document.getElementById('password1').value ==
-      document.getElementById('password2').value) {
-      document.getElementById('message').style.color = 'green';
-      document.getElementById('message').innerHTML = '✔';
+    if (document.getElementById('password1').value == document.getElementById('password2').value) {
+        document.getElementById('message').style.color = 'green';
+        //document.getElementById('message').innerText = '✔';
+        document.getElementById('message').innerText = '';
     } else {
-      document.getElementById('message').style.color = 'red';
-      document.getElementById('message').innerHTML = 'x';
+        document.getElementById('message').style.color = 'red';
+        document.getElementById('message').innerText = 'x';
     }
-  }
+}
