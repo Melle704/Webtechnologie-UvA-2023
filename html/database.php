8520dc50891c981c44524224f665d29b70b7f3d8
Nicolas Mazzon <nicolasmazzon549@gmail.com>
2023-01-25 15:33:24 +0100

    Servaes search added to database.

--- html/database.php
+++ html/database.php
@@ -47,12 +47,41 @@ $last_page = intdiv(intval($last_page), $cards_per_page) + 1;
 
     <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
 	<link rel="stylesheet" type="text/css" href="/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/css/form.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
 </head>
 
 <body>
 
 <?php include_once "header.php"; ?>
+
+<div class="box">
+    <div class="box-row box-light">
+        <b>Simple search</b>
+    </div>
+    <div class="box-row form">
+    <form action="" method="GET">
+            <b>card name</b>
+            <input type="text" id="card_name">
+            <br><br>
+            <b>oracle text</b>
+            <input type="text" id="oracle_text">
+            <br><br>
+            <b>card type</b>
+            <input type="text" id="card_type">
+            <br><br>
+            <b>colors</b>
+            <input class="white_checkbox" type="checkbox" id="white" value="white">
+            <input class="blue_checkbox" type="checkbox" id="blue" value="blue">
+            <input class="black_checkbox" type="checkbox" id="black" value="black">
+            <input class="red_checkbox" type="checkbox" id="red" value="red">
+            <input class="green_checkbox" type="checkbox" id="green" value="green">
+            <input class="colorless_checkbox" type="checkbox" id="colorless" value="colorless">
+            <br><br>
+            <input type="submit" name="submit" value="Search">
+    </form>
+    </div>
+</div>
 
 <div class="box box-row box-container">
     <?php
