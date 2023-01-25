1f561e3e0c4240f8b55a9c24d1c2f3513001aee0
Nicolas Mazzon <nicolasmazzon549@gmail.com>
2023-01-25 14:42:08 +0100

    Database page navigation.

--- html/include/redirect.php
+++ html/include/redirect.php
@@ -10,7 +10,7 @@ if (!isset($redirect_title) || !isset($redirect_msg)) {
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>MTG | <?php echo $redirect_title; ?></title>
+    <title>MTG | <?= $redirect_title ?></title>
 
     <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
 	<link rel="stylesheet" type="text/css" href="/css/style.css">
@@ -24,7 +24,7 @@ if (!isset($redirect_title) || !isset($redirect_msg)) {
         </div>
 
         <div class="box-row">
-            <?php echo $redirect_msg; ?>
+            <?= $redirect_msg ?>
             <br>
             <br>
             <u>Do not refresh the page during this process.</u>
