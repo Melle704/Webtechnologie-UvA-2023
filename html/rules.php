2c2d858e159eb8ecbe4155f15866d8c72f51e8b6
Nicolas Mazzon <nicolasmazzon549@gmail.com>
2023-01-25 02:19:02 +0100

    Flavor text + more on home page.

---
+++ html/rules.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>MTG | About us</title>
+
+    <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
+</head>
+
+<body>
+    <?php include_once "header.php";?>
+
+    <div class="box">
+        <div class="box-row box-light">
+            <b>Rules</b>
+        </div>
+        <div class="box-row">
+            &#8226; there are no strict rules
+            <br>
+            &#8226; use your head
+            <br>
+            &#8226; keep an open mind
+        </div>
+    </div>
+
+    <?php include_once "footer.php"; ?>
+</body>
+
+</html>
