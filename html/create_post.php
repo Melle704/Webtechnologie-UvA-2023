ca3d19b04ac6823af78cbc11421c4910c6b45d08
Kas Visser <kas.visser@outlook.com>
2023-01-28 21:29:31 +0100

    Creating a post works, but still some bugs.

--- html/create_post.php
+++ html/create_post.php
@@ -5,15 +5,35 @@ session_start();
 include_once "include/common.php";
 include_once "include/db.php";
 
+// $thread_id =
+
+
+$user_id = $_SESSION["id"];
+
+if (isset($_POST["submit"])) {
+    $title = htmlspecialchars(trim($_POST["title"]));
+    $text = htmlspecialchars(trim($_POST["content"]));
+
+    // Make sure the input is valid
+    validate_predicates(["Title should be at least 2 characters", strlen($title) >= 2]);
+    validate_predicates(["Title should not exceed 124 characters", strlen($title) <= 124]);
+    validate_predicates(["Content should not exceed 4096 characters", strlen($text) <= 4096]);
+
+    $sql = "INSERT INTO forum_threads (user_id, title, thread_content) VALUES (?, ?, ?)";
+    query_execute($db, $sql, "iss", $user_id, $title, $text);
+
+    header("Location: " . $_SERVER["REQUEST_URI"]);
+    exit;
+}
 ?>
+
 <div class="box box-row">
     <div class="create-title">
         Create a post!
     </div>
-    <textarea class="textarea-title" rows="1" maxlength="124" placeholder="Title"></textarea>
-
-    <textarea maxlength="4096" placeholder="Text (optional)"></textarea>
-
-    <input type="submit"></input>
+    <form id="new-thread-form" method="post" onsubmit="location.reload()">
+        <textarea class="textarea-title" name="title" rows="1" maxlength="124" placeholder="Title"></textarea>
+        <textarea class="textarea-content" name="content" maxlength="4096" placeholder="Text (optional)"></textarea>
+        <input type="submit" name="submit"></input>
+    </form>
 </div>
-<?php
