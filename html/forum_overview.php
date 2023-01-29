3614a65cd3c613325569ef0a40f51e83966a4c0c
Kas Visser <kas.visser@outlook.com>
2023-01-29 15:44:37 +0100

    Added 'sort by' functionality to change how to posts are sorted.

--- html/forum_overview.php
+++ html/forum_overview.php
@@ -2,12 +2,38 @@
 
 session_start();
 
-// ensure you can't reach the forum page if you're not logged in
+// Ensure you can't reach the forum page if you're not logged in.
 if (!isset($_SESSION["id"])) {
     header("Location: /index.php");
     exit;
 }
+
+include_once "include/common.php";
+include_once "include/db.php";
+
+// Sort by highest score on standerd.
+$sortBy = "score";
+if (isset($_GET["sortBy"])) {
+  $sortBy = $_GET["sortBy"];
+}
+
+// Create the query for how to sort.
+$query = "SELECT * FROM forum_threads";
+if ($sortBy === "score") {
+  $query .= " ORDER BY score";
+} else if ($sortBy === "score-desc") {
+    $query .= " ORDER BY score DESC";
+
+} else if ($sortBy === "date") {
+  $query .= " ORDER BY date";
+} else if ($sortBy === "date-desc") {
+    $query .= " ORDER BY date DESC";
+}
+$query .= " LIMIT 20";
+
+$threads = query_execute_unsafe($db, $query);
 ?>
+
 <div class="box">
     <div class="box-row box-light">
         <div class="post-title box-title">
@@ -17,23 +43,31 @@ if (!isset($_SESSION["id"])) {
             A space to ask questions and discuss Magic!
             <br>
             <a href="forumrules.php">Forum rules</a>
+            <select id="sort-select" style="background-color: #323232; float: right">
+                <option value="score" <?= ($sortBy === "score") ? "selected" : "" ?>>Sort by highest score</option>
+                <option value="score-desc" <?= ($sortBy === "score-desc") ? "selected" : "" ?>>Sort by lowest score</option>
+                <option value="date" <?= ($sortBy === "date") ? "selected" : "" ?>>Sort by oldest</option>
+                <option value="date-desc" <?= ($sortBy === "date-desc") ? "selected" : "" ?>>Sort by newest</option>
+            </select>
         </p>
+
+        <script>
+        const select = document.getElementById("sort-select");
+        select.addEventListener("change", function() {
+            window.location.href = "/forum.php?sortBy=" + select.value;
+        });
+        </script>
     </div>
 
     <div class="box-row">
-<?php
-include_once "include/common.php";
-include_once "include/db.php";
+        <?php foreach ($threads as $thread): ?>
+        <?php
+            $date = format_datetime($thread["date"]);
 
-$threads = query_execute_unsafe($db, "SELECT * FROM forum_threads ORDER BY score LIMIT 10");
-
-foreach ($threads as $thread):
-    $date = format_datetime($thread["date"]);
-
-    $thread_id = $thread["id"];
-    $sql = "SELECT SUM(thread_id=$thread_id) FROM forum_posts";
-    $comment_count = query_execute_unsafe($db, $sql)[0][0];
-?>
+            $thread_id = $thread["id"];
+            $sql = "SELECT SUM(thread_id=$thread_id) FROM forum_posts";
+            $comment_count = query_execute_unsafe($db, $sql)[0][0];
+        ?>
         <div class="preview-box">
             <a href="/post.php?id=<?= $thread_id ?>">
                 <div class="preview-title"><b><?= $thread["title"] ?></b></div>
@@ -43,6 +77,6 @@ foreach ($threads as $thread):
                 </div>
             </a>
         </div>
-<?php endforeach ?>
+        <?php endforeach ?>
     </div>
 </div>
