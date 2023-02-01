962a0c8e5f0c0df7dc4631bfc704289cdd0115ea
Kas Visser <kas.visser@outlook.com>
2023-02-01 14:13:55 +0100

    Replaced the rules with the forum rules.

--- html/rules.php
+++ html/rules.php
@@ -5,10 +5,10 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>MTG | About us</title>
+    <title>MTG | Forum</title>
 
     <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
-	<link rel="stylesheet" type="text/css" href="/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
 </head>
 
 <body>
@@ -16,15 +16,16 @@
 
     <div class="box">
         <div class="box-row box-light">
-            <b>Rules</b>
+            <b>Forum rules</b>
         </div>
-        <div class="box-row">
-            <ul>
-                <li>There are no strict rules</li>
-                <li>Use your head</li>
-                <li>Keep an open mind</li>
-            </ul>
-        </div>
+        <ol type="1">
+            <li>Be respectfull to eachother, our forum is a place of community, not a place of hate.</li>
+            <li>No ban evasion, spamming or fraud.</li>
+            <li>Respect the privacy of other users. No posting of personal information without consent.</li>
+            <li>You don't have to use your real name, but don't impersonate other people.</li>
+            <li>Keep the posts and comments legal, don't post any illegal content.</li>
+            <li>Don't break our site or try anything to break our site. <b>We will find you!</b></li>
+        </ol>
     </div>
 
     <?php include_once "footer.php"; ?>
